d16106cbb72dd778fe17133ffb61fe90763defc5
root <root@ip-172-31-47-219.us-west-2.compute.internal>
2025-05-07 11:01:58 +0000

    Pipeline Issue Solve

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,21 +1,32 @@
-pipeline{
+pipeline {
     agent any
-    stages{
-        stage("Code-Pull"){
-            steps{
+
+    stages {
+        stage("Code-Pull") {
+            steps {
                 git branch: 'dev', url: 'https://github.com/pratik-85/project-frontend.git'
             }
         }
-        stage("Code-Build"){
-            steps{
-                sh '''npm install
-                   ng build '''
+
+        stage("Code-Build") {
+            steps {
+                sh '''
+                    npm install
+                    ng build
+                '''
             }
         }
-        stage("Code-Deploy"){
-            steps{
-                sh 'aws s3 cp --recursive dist/angular-frontend s3://frontend-cbz-07-05-2025/'
+
+        stage("Code-Deploy") {
+            steps {
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding',
+                                  credentialsId: 'aws-creds',
+                                  accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                                  secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                    sh 'aws s3 cp --recursive dist/angular-frontend s3://frontend-cbz-07-05-2025/'
+                }
             }
         }
     }
 }
+
